d151984ef5a4a6d23f3cf2dbcf8337a24978c9ed
hauck29 <anthony.hauck@aol.com>
2021-10-18 15:06:41 -0700

    added some files

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const app = express();
 // view engine setup
 app.set('view engine', 'pug');
 
+
+app.use(usersRouter);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
