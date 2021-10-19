373c8a77aa43e15a530ac043a652c7dd6542e8cb
JamesRR91 <jamesrusr@gmail.com>
2021-10-19 09:31:10 -0700

    created user seed, fixed movieid error

--- db/migrations/20211018203605-create-review.js
+++ db/migrations/20211018203605-create-review.js
@@ -17,7 +17,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references:{model:'Users'}
       },
-      movieid: {
+      horrormovieid: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references:{model:'HorrorMovies'}
