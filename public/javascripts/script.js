1696646b44b074933f7815510dbb8c7c4d890cb8
JamesRR91 <jamesrusr@gmail.com>
2021-10-21 11:15:09 -0700

    updownvotes ver 1 ready

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,35 @@
-// const { db } = require("../db/models");
+    // let countVotes=0;
+    // let score=document.querySelector('.score');
+    //  score.innerText=countVotes;
+
+    // const upVoteButton = document.querySelector('#upvote')
+
+    // const downVoteButton = document.querySelector('#downvote')
+
+    // upVoteButton.addEventListener('click', () => {
+    //     document.querySelector('.score').innerText++;
+    //     localStorage.setItem('score', countVotes);
+    // })
+
+    // downVoteButton.addEventListener('click', () => {
+    //     document.querySelector('.score').innerText--;
+    // })
+
+    const countVotes = 0;
+
+let  score = document.querySelector('.score')
+score.innerText = countVotes;
+
+let upVoteButton = document.querySelector('#upvote');
+upVoteButton.addEventListener('click', (e) => {
+    score.innerText++
+})
+
+let downVoteButton = document.querySelector('#downvote');
+downVoteButton.addEventListener('click', (e) => {
+    score.innerText--
+})
+
 
 // let addToWatchlist = document.querySelectorAll('.btn1')
 
